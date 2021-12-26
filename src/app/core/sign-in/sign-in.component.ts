df857e0e50558cd7883abcb0ec47afa9f98f7611
viraj02 <virujoshi02@gmail.com>
2021-12-26 22:56:49 +0530

    copied login code in signin

--- src/app/core/sign-in/sign-in.component.ts
+++ src/app/core/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private routing: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.loginForm = new FormGroup(
+      {
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$')]),
+        password: new FormControl('', Validators.required)
+      }
+    );
+  }
+
+  // tslint:disable-next-line: typedef
+  get formControls() {
+    return this.loginForm.controls;
+  }
+
+  submitUserDetails(): void {
+    console.log(this.formControls);
+    if (!this.loginForm.valid) {
+      console.log('Unsuccessful Login');
+      return;
+    }
+    console.log('Login Successfully');
+  }
+
+  navigate(url: string): void {
+    this.routing.navigate([`${url}`]);
   }
 
 }
