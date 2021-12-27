c3fcd15425659c54551e0098e3b05ee95cb9a5cc
viraj02 <virujoshi02@gmail.com>
2021-12-27 20:33:21 +0530

    created user interface

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '@app/_models/user';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+
+}
