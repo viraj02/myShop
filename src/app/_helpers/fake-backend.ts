6f561fc56760cee4d8be16f03756338e00e85f34
viraj02 <virujoshi02@gmail.com>
2021-12-28 01:14:29 +0530

    created fake backend to generate jwt till actual backend is not created

---
+++ src/app/_helpers/fake-backend.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { User } from '@app/_models/user';
+import { delay, dematerialize, materialize, mergeMap } from 'rxjs/operators';
+
+const users: User[] = [{ id: 1, username: 'test', password: 'test', firstName: 'Test', lastName: 'User' }];
+
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+
+  constructor() { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const { url, method, headers, body } = request;
+
+    // wrap in delayed observable to simulate server api call
+    return of(null)
+      .pipe(mergeMap(handleRoute))
+      .pipe(materialize())
+      .pipe(delay(500))
+      .pipe(dematerialize());
+
+    function handleRoute(): Observable<HttpEvent<any>> {
+      switch (true) {
+        case url.endsWith('/users/authenticate') && method === 'POST':
+          return authenticate();
+        case url.endsWith('/users') && method === 'GET':
+          return getUsers();
+        default:
+          // pass through any requests not handled above
+          return next.handle(request);
+      }
+    }
+
+    // route functions
+
+    function authenticate(): Observable<HttpResponse<any>> {
+      const { username, password } = body;
+      const user = users.find(x => x.username === username && x.password === password);
+      if (!user) { return error('Username or password is incorrect'); }
+      return ok({
+        id: user.id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        token: 'fake-jwt-token'
+      });
+    }
+
+    function getUsers(): Observable<HttpResponse<any>> {
+      if (!isLoggedIn()) { return unauthorized(); }
+      return ok(users);
+    }
+
+    // helper functions
+
+    function ok(body?): Observable<HttpResponse<any>> {
+      return of(new HttpResponse({ status: 200, body }));
+    }
+
+    function error(message): Observable<never> {
+      return throwError({ error: { message } });
+    }
+
+    function unauthorized(): Observable<never> {
+      return throwError({ status: 401, error: { message: 'Unauthorised' } });
+    }
+
+    function isLoggedIn(): boolean {
+      return headers.get('Authorization') === 'Bearer fake-jwt-token';
+    }
+
+
+  }
+
+}
+
+
+export let fakeBackendProvider = {
+  // use fake backend in place of Http service for backend-less development
+  provide: HTTP_INTERCEPTORS,
+  useClass: FakeBackendInterceptor,
+  multi: true
+};
+
